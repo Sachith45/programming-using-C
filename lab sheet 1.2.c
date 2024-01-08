5af81a0348297f348b19b05806b33878970a1dea
sachith avintha <sachithavintha35@gmail.com>
2024-01-08 10:47:32 +0530

    add labsheet 1.2

---
+++ lab sheet 1.2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Display the output
+    printf("\"Hello\"\n");
+    printf("what are you doing ?\n");
+    printf("'my first day'\n");
+
+    return 0;
+}
+
