c11407fbf4a364779cecc10f0a87c04b1ad9cb62
sachith avintha <sachithavintha35@gmail.com>
2024-01-09 22:02:56 +0530

    add labsheet 2.4

---
+++ labsheet 2.4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    double x,result;
+    int N;
+    x=1.3;
+    N=5;
+    result=pow(x,N);//this pow function use to get power any number
+
+    printf("result=%.2f",result);
+
+    return 0;
+}
