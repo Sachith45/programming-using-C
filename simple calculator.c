6670805f6279f7d218e3f0652ff46da0eb45c1af
sachith avintha <sachithavintha35@gmail.com>
2024-01-13 17:33:36 +0530

    add simple calulator code

---
+++ simple calculator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    float a,b,result;
+
+    printf("Enter operote you want:");
+    scanf("%c",&c);
+
+    printf("Enter first value:");
+    scanf("%f",&a);
+
+    printf("Enter second value:");
+    scanf("%f",&b);
+
+
+    if(c=='+'){
+        result=a+b;
+    }
+
+     else if(c=='-'){
+        result=a-b;
+    }
+
+     else if(c=='*'){
+        result=a*b;
+    }
+
+     else{
+        result=a/b;
+    }
+
+    printf("result=%.2f",result);
+
+
+
+
+    return 0;
+
+}
